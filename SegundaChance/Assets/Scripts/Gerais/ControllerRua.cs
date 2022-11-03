afb7090e4b234f2fb420527f533ac4f5b130e389
ImBrunoHG <38635661+BrunimMS@users.noreply.github.com>
2022-11-03 12:12:10 -0300

    Coisinhas

--- SegundaChance/Assets/Scripts/Gerais/ControllerRua.cs
+++ SegundaChance/Assets/Scripts/Gerais/ControllerRua.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class ControllerRua : MonoBehaviour
 {
     [SerializeField] Player p;
+    [SerializeField] bool b;
     // Start is called before the first frame update
     void Start()
     {
         p.anim.SetBool("Uniforme", true);
+        if (b)
+        {
+            p.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
